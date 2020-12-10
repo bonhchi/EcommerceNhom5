193012970dba620991d0f7a0556d5dfa955ecdc8
bonhchi <phu13325@gmail.com>
2020-12-10 10:17:32 +0700

    Add Paypal

--- PCWeb/Controllers/CartController.cs
+++ PCWeb/Controllers/CartController.cs
@@ -22,13 +22,13 @@ namespace PCWeb.Controllers
         private readonly string secretKey;
         private const double exchange = 23220;
         private const string currency = "USD";
-
+ 
         public CartController(DataContext dataContext, IConfiguration config)
         {
             this.dataContext = dataContext;
             this.config = config;
-            clientId = config["PaypalSetting:ClientId"];
-            secretKey = config["PaypalSetting:SecretKey"];
+            clientId = config["PaypalSettings:ClientId"];
+            secretKey = config["PaypalSettings:SecretKey"];
         }
         public IActionResult Index()
         {
@@ -128,37 +128,115 @@ namespace PCWeb.Controllers
             {
                 if(answer == "paypal")
                 {
-                    Models.Order orderTemp = new Models.Order
+                    //var productList = dataContext.Products.ToList();
+                    var environment = new SandboxEnvironment(clientId, secretKey);
+                    var client = new PayPalHttpClient(environment);
+                    var itemList = new ItemList()
                     {
-                        OrderDate = DateTime.Now,
-                        Phone = order.Phone,
-                        Address = order.Address,
-                        Email = order.Email,
-                        CusName = order.CusName,
-                        OrderConditionId = 1,
-                        PaymentMethodId = 2
+                        Items = new List<Item>()
                     };
-                    dataContext.Orders.Add(orderTemp);
-                    dataContext.SaveChanges();
-                    var query = dataContext.Orders.FirstOrDefault(p => p.OrderId == orderTemp.OrderId);
+                    var total = cart.Sum(item => item.Product.ProductPrice * item.Quantity);
                     foreach (var item in cart)
                     {
-                        dataContext.OrderDetails.Add(new OrderDetail()
+                        itemList.Items.Add(new Item()
                         {
-                            OrderId = query.OrderId,
-                            ProductId = item.Product.ProductId,
-                            Quantity = item.Quantity,
+                            Name = item.Product.ProductName,
+                            Currency = currency,
+                            Price = Math.Round(item.Product.ProductPrice / exchange, 2).ToString(),
+                            Quantity = item.Quantity.ToString(),
+                            Sku = "sku",
+                            Tax = "0"
                         });
-                        Product product = dataContext.Products.FirstOrDefault(p => p.ProductId == item.Product.ProductId);
-                        product.ProductQuantity -= item.Quantity;
+
                     }
-                    dataContext.SaveChanges();
-                    //Bug
-                    cart.Clear();
-                    TempData["check"] = query.OrderId;
-                    SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
-                    Task<IActionResult> actionResult = PaypalCheckout();
-                    return await actionResult;
+                    var paypalOrderId = DateTime.Now.Ticks;
+                    var hostname = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}";
+                    var payment = new Payment()
+                    {
+                        Intent = "sale",
+                        Transactions = new List<Transaction>()
+                    {
+                    new Transaction ()
+                    {
+                        Amount = new Amount()
+                        {
+                            Total = Math.Round(total/exchange,2).ToString(),
+                            Currency = currency,
+                            Details = new AmountDetails
+                            {
+                                Tax = "0",
+                                Shipping = "0",
+                                Subtotal = Math.Round(total/exchange,2).ToString()
+                            }
+                        },
+                        ItemList = itemList,
+                        Description = $"Invoice #{paypalOrderId}",
+                        InvoiceNumber = paypalOrderId.ToString()
+                    }
+                },
+                        //direct đến trang nào đó trả lệnh
+                        RedirectUrls = new RedirectUrls()
+                        {
+                            CancelUrl = $"{hostname}/Cart/Fail",
+                            ReturnUrl = $"{hostname}/Cart/Confirm",
+                        },
+                        Payer = new Payer()
+                        {
+                            PaymentMethod = "paypal"
+                        }
+                    };
+                    PaymentCreateRequest request = new PaymentCreateRequest();
+                    request.RequestBody(payment);
+                    try
+                    {
+                        var response = await client.Execute(request);
+                        var statusCode = response.StatusCode;
+                        Payment result = response.Result<Payment>();
+                        var links = result.Links.GetEnumerator();
+                        string paypalRedirectUrl = null;
+                        while (links.MoveNext())
+                        {
+                            LinkDescriptionObject link = links.Current;
+                            if (link.Rel.ToLower().Trim().Equals("approval_url"))
+                                paypalRedirectUrl = link.Href;
+                        }
+                        Models.Order orderTemp = new Models.Order
+                        {
+                            OrderDate = DateTime.Now,
+                            Phone = order.Phone,
+                            Address = order.Address,
+                            Email = order.Email,
+                            CusName = order.CusName,
+                            OrderConditionId = 1,
+                            PaymentMethodId = 2
+                        };
+                        dataContext.Orders.Add(orderTemp);
+                        dataContext.SaveChanges();
+                        var query = dataContext.Orders.FirstOrDefault(p => p.OrderId == orderTemp.OrderId);
+                        foreach (var item in cart)
+                        {
+                            dataContext.OrderDetails.Add(new OrderDetail()
+                            {
+                                OrderId = query.OrderId,
+                                ProductId = item.Product.ProductId,
+                                Quantity = item.Quantity,
+                            });
+                            Product product = dataContext.Products.FirstOrDefault(p => p.ProductId == item.Product.ProductId);
+                            product.ProductQuantity -= item.Quantity;
+                        }
+                        dataContext.SaveChanges();
+                        //Bug
+                        cart.Clear();
+                        TempData["check"] = query.OrderId;
+                        SessionHelper.SetObjectAsJson(HttpContext.Session, "cart", cart);
+                        return Redirect(paypalRedirectUrl);
+                    }
+                    catch (HttpException httpException)
+                    {
+                        var statusCode = httpException.StatusCode;
+                        var debugId = httpException.Headers.GetValues("PayPal-Debug-Id").FirstOrDefault();
+                        return Redirect("/Cart/Fail");
+                    } 
                 }
                 else
                 {
@@ -169,7 +247,8 @@ namespace PCWeb.Controllers
                         Address = order.Address,
                         Email = order.Email,
                         CusName = order.CusName,
-                        PaymentMethodId = 1
+                        PaymentMethodId = 1,
+                        OrderConditionId = 1
                     };
                     dataContext.Orders.Add(orderTemp);
                     dataContext.SaveChanges();
@@ -203,97 +282,6 @@ namespace PCWeb.Controllers
                     return View();
                 return View(order);
             }
-        }
-        public async Task<IActionResult> PaypalCheckout()
-        {
-            var environment = new SandboxEnvironment(clientId, secretKey);
-            var client = new PayPalHttpClient(environment);
-            List<OrderDetail> cart = SessionHelper.GetObjectFromJson<List<OrderDetail>>(HttpContext.Session, "cart");
-            var itemList = new ItemList()
-            {
-                Items = new List<Item>()
-            };
-            var total = cart.Sum(item => item.Product.ProductPrice * item.Quantity);
-            foreach(var item in cart)
-            {
-                itemList.Items.Add(new Item()
-                {
-                    Name = item.Product.ProductName,
-                    Currency = currency,
-                    Price = Math.Round(item.Product.ProductPrice / exchange, 2).ToString(),
-                    Quantity = item.Quantity.ToString(),
-                    Sku = item.Product.ProductSeries,
-                    Tax = "0"
-                });
-
-            }
-            int orderId = dataContext.Orders.Count() + 1;
-            var hostname = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}";
-            var payment = new Payment()
-            {
-                Intent = "sale",
-                Transactions = new List<Transaction>()
-                {
-                    new Transaction ()
-                    {
-                        Amount = new Amount()
-                        {
-                            Total = total.ToString(),
-                            Currency = currency,
-                            Details = new AmountDetails
-                            {
-                                Tax = "0",
-                                Shipping = "0",
-                                Subtotal = total.ToString()
-                            }
-                        },
-                        ItemList = itemList,
-                        Description = $"Invoice #{orderId}",
-                        InvoiceNumber = orderId.ToString()
-                    }
-                },
-                //direct đến trang nào đó trả lệnh
-                RedirectUrls = new RedirectUrls()
-                {
-                    CancelUrl = $"{hostname}/Cart/Fail",
-                    ReturnUrl = $"{hostname}/Cart/Checkout",
-                },
-                Payer = new Payer()
-                {
-                    PaymentMethod = "paypal"
-                }
-            };
-            PaymentCreateRequest request = new PaymentCreateRequest();
-            request.RequestBody(payment);
-            try
-            {
-                var response = await client.Execute(request);
-                var statusCode = response.StatusCode;
-                Payment result = response.Result<Payment>();
-                var links = result.Links.GetEnumerator();
-                string paypalRedirectUrl = null;
-                while(links.MoveNext())
-                {
-                    LinkDescriptionObject link = links.Current;
-                    if (link.Rel.ToLower().Trim().Equals("approval_url"))
-                        paypalRedirectUrl = link.Href;
-                }
-                return Redirect(paypalRedirectUrl);   
-            }
-            catch(HttpException httpException)
-            {
-                var statusCode = httpException.StatusCode;
-                var debugId = httpException.Headers.GetValues("PayPal-Debug-Id").FirstOrDefault();
-                return Redirect("/Cart/Fail");
-            }
-        }
-        [HttpPost]
-        public IActionResult TestCheckbox(string answer)
-        {
-            if (answer == "1")
-                return Redirect("~/Home/Index");
-            else
-                return Redirect("~/Index");
         }
         public IActionResult Fail()
         {
