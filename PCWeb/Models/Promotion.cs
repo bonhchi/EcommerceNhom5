5a96273aeae7906e09de31f3ca9cf258ed7dfcdc
bonhchi <phu13325@gmail.com>
2020-12-15 18:40:16 +0700

    Change Database (Not Migration)

--- PCWeb/Models/Promotion.cs
+++ PCWeb/Models/Promotion.cs
@@ -12,6 +12,14 @@ namespace PCWeb.Models
         [DisplayName("Tên khuyến mãi")]
         [Required(ErrorMessage = "Khuyến mãi không được để trống")]
         public string PromotionName { get; set; }
+        [DisplayName("Mặt hàng áp dụng")]
+        public string PromotionApply { get; set; }
+        [RegularExpression(@"^[A-Z]+[0-9]+$", ErrorMessage = "Mã giảm giá không hợp lệ")]
+        [MinLength(8, ErrorMessage = "Mã giám giá ít nhất 8 kí tự")]
+        [DisplayName("Mã giảm giá")]
+        public string PromotionCode { get; set; }
+        [DisplayName("Cần kích hoạt mã giảm giá")]
+        public bool PromotionCodeNeed { get; set; }
         public Product Product { get; set; }
     }
 }
